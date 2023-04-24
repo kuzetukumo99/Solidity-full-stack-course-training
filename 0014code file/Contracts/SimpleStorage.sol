c84447d3d32fcbcb76ed429a81ccd21a962091fe
SHUICHI YAMANAKA <103573257+kuzetukumo99@users.noreply.github.com>
2023-04-24 22:16:26 +0900

    Training Code keep

---
+++ 0014code file/Contracts/SimpleStorage.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8; //0.8.18　ダブルスラッシュはコメント機能
+
+contract SimpleStorage{
+    uint256 favoriteNumber;// ０に初期化する。uint256 favoriteNumber == 0と等しい。
+}
